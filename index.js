0c0d27d15da4f06a67e53ee62dae0c7796f1a17f
amberm <nekosenpai@gmail.com>
2019-02-21 11:17:27 -0700

    new code for posting

--- index.js
+++ index.js
@@ -7,6 +7,8 @@ const server = require('./api/server.js');
 // heroku will have the PORT environment variable set
 const port = process.env.PORT || 5000;
 
+
+
 // we can now use that port, if set up by heroku or read from .env or 5000 as a default if not set
 server.listen(port, () => {
   console.log(`\n*** Server Running on http://localhost:${port} ***\n`);
